d1368d094a9718d241b08c382fa7dc92382be2da
ryota629 <ryo721.tddc_bluer@ezweb.ne.jp>
2020-07-28 20:42:07 +0900

    change_7/28

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Folder;
+use App\Task;
+
+class TaskController extends Controller
+{
+    public function index(int $id){
+        $folders = Folder::all();
+
+        $current_folders = Folder::find($id);
+
+        $tasks = Task::where('folder_id', $current_folders->id)->get();
+
+        return view('tasks/index',[
+            'folders' => $folders,
+            'current_folder_id' => $current_folders->$id,
+            'tasks' => $tasks,
+        ]);
+    }
+}
---
+++ app/Task.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Carbon\Carbon;
+
+class Task extends Model
+{
+    //
+    const STATUS = [
+        1 => ['label' => '未着手'],
+        2 => ['label' => '着手'],
+        3 => ['label' => '完了'],
+    ];
+
+    public function getStatusLabelAttribute(){
+        $status = $this->attributes['status'];
+
+        if(!isset(self::STATUS[$status])){
+            return '';
+        }
+
+        return self::STATUS[$status]['class'];
+    }
+
+    public function getFormattedDueDateAttribute(){
+        return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])->format('Y/m/d');
+    }
+}
